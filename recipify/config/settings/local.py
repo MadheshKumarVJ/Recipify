82249889832c30175235a935113dd684bb600732
MadheshKumarVJ <112080679+MadheshKumarVJ@users.noreply.github.com>
2022-12-30 16:29:56 +0530

    Initialize Recipify Project (#1)

---
+++ recipify/config/settings/local.py
@@ -0,0 +1,25 @@
+from config.env import env
+
+from .base import *
+
+DEBUG = env("DEBUG")
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+MIDDLEWARE += [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("DATABASE_ENGINE"),
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+    }
+}
