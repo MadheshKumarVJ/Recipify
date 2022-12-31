e9c5379a2f465264ec7d11540e1de61ec6558caa
MadheshKumarVJ <112080679+MadheshKumarVJ@users.noreply.github.com>
2023-01-01 00:14:26 +0530

    Change .env and add dummy test (#3)
    * Test
    
    * Add sample env
    
    * Make local setting as default in manage.py
    
    * Update test path
    
    * Update test runner
    
    * update
    
    * fix typo
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update env varibles

--- config/env.py
+++ config/env.py
@@ -1,4 +1,9 @@
+import os
+from pathlib import Path
+
 import environ
 
 env = environ.Env()
-environ.Env.read_env()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
--- config/settings/local.py
+++ config/settings/local.py
@@ -21,5 +21,7 @@ DATABASES = {
         "NAME": env("DATABASE_NAME"),
         "USER": env("DATABASE_USER"),
         "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
     }
 }
