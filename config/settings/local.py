3b847245295c4680b8436eed8e9fac29df1a51ff
MadheshKumarVJ <vjmadhesh@testpress.in>
2022-12-31 08:04:37 +0530

    Update env varibles

--- config/settings/local.py
+++ config/settings/local.py
@@ -21,7 +21,7 @@ DATABASES = {
         "NAME": env("DATABASE_NAME"),
         "USER": env("DATABASE_USER"),
         "PASSWORD": env("DATABASE_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "5432",
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
     }
 }
